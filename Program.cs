e1e1f080d192af223868d5963dead4301f3e2589
Kaushik Donthi <kaushikdr@ucla.edu>
2021-05-02 20:19:46 -0700

    Created Block and Blockchain Classes

--- Program.cs
+++ Program.cs
@@ -2,6 +2,33 @@
 
 namespace Blockchain
 {
+    HashAlgorithm sha = SHA256.Create();
+    class Block
+    {
+        String      hash;
+        String      prev_hash;
+        int         index;
+        DateTime    timestamp;
+        String      data;
+
+        public void init(String _hash, String _prev_hash, int _index, DateTime _timestamp, String _data)
+        {
+            prev_hash = _prev_hash;
+            index = _index;
+            data = _data;
+            hash = sha. //we need to create this hash anew
+            timestamp = DateTime.Now;
+        }
+    }
+    class Chain
+    {
+        Block head;
+        Block last_block;
+        public void add_block()
+        {
+
+        }
+    }
     class Program
     {
         static void Main(string[] args)
