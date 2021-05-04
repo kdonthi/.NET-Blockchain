c742dda3bd3ed3112981651181fa5d2b35dedd29
Kaushik Donthi <kaushikdr@ucla.edu>
2021-05-03 20:45:53 -0700

    Update Program.cs
    Got proof of work to work for one zero at the end difficulty

--- Program.cs
+++ Program.cs
@@ -10,6 +10,8 @@ namespace Blockchain
     public class Block
     {
         public String               hash;
+        public String               nonce;
+        //public String               intermediateNonce;
         public String               hash_input;
         public int                  hash_len;
         public String               prev_hash;
@@ -25,18 +27,49 @@ namespace Blockchain
             stringOfZeros = "";
             for (int i = 0; i < difficulty; i++)
                 stringOfZeros += "0";
+            Console.WriteLine("String of Zeros: {0}", stringOfZeros);
             prev_hash = _prev_hash;
             index = _index;
             data = _data;
-            hash_input = $"Block at {index} index. Prev_hash: {prev_hash ?? ""} Data: {data}";
-            hash = byte_array_to_string(sha.ComputeHash(Encoding.ASCII.GetBytes(hash_input)));
-            hash_len = hash.Length;
-            while (ft_strcmp(hash.Substring(hash_len - 4), stringOfZeros) == 1)
-
             //Console.WriteLine("Index: {0}, Hash: {1}", _index, hash);
             //Console.WriteLine("Previous Hash: {0}, Current Hash: {1}", _prev_hash, hash);
             timestamp = DateTime.Now;
             nextBlock = null;
+            hash_input = $"Block at {index} index. Prev_hash: {prev_hash ?? ""} Data: {data}";
+            hash = byte_array_to_string(sha.ComputeHash(Encoding.ASCII.GetBytes(hash_input)));
+            hash_len = hash.Length;
+            nonce = "";
+            //Console.WriteLine("Original Hash: {0}", hash);
+            if (ft_strcmp(hash.Substring(hash_len - difficulty), stringOfZeros) == 1)
+            {
+                for (int i = 0; i < 30; i++)
+                {
+                    //Console.WriteLine("Here!");
+                    for (int j = -1; j < 128; j++)
+                    {
+                        if (j == -1)
+                        {
+                            nonce = "" + (char) i;
+
+                        }
+                        else
+                        {
+                            nonce = "" + (char) i + (char) j;
+                        }
+                        hash = byte_array_to_string(sha.ComputeHash(Encoding.ASCII.GetBytes(hash_input + nonce)));
+                        //Console.WriteLine("Hash: {0}, Nonce: {1}, i: {2}, j: {3}", hash, nonce, i, j);
+                        
+                        hash_len = hash.Length;
+                        //Console.WriteLine()
+                        if (ft_strcmp(hash.Substring(hash_len - difficulty), stringOfZeros) == 0)
+                        {
+                            Console.WriteLine("Here 2");
+                            return;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("Here 3");
         }
 
         public static int ft_strcmp(String s1, String s2)
@@ -92,12 +125,14 @@ namespace Blockchain
 
     class Chain
     {
+        public static int difficulty;
         public List<Block> chaind = new List<Block>();
         static Block last_block;
-        public void init_chain(String data)
+        public void init_chain(String data, int _difficulty)
         {
+            difficulty = _difficulty; 
             Block head = new Block();
-            head.init(null, 0, data);
+            head.init(null, 0, data, difficulty);
             chaind.Add(head);
             last_block = head;
         }
@@ -105,7 +140,7 @@ namespace Blockchain
         {
             last_block.nextBlock = new Block();
             last_block = last_block.nextBlock;
-            last_block.init(_prev_hash, _index, _data);
+            last_block.init(_prev_hash, _index, _data, difficulty);
             chaind.Add(last_block);
             //Console.WriteLine("Index: {0}, Index in List: {1}", _index, chaind[_index].index);
             //Console.WriteLine("Previous Hash: {0}, Current Hash: {1}", _prev_hash, last_block.hash);
@@ -118,16 +153,17 @@ namespace Blockchain
     {
         static void Main(string[] args)
         {
-            /*
+            int difficulty = 1; //how many zeros we want at end of hash
             Chain blockchain = new Chain();
-            blockchain.init_chain("First transaction");
+            blockchain.init_chain("First transaction!", difficulty);
             blockchain.add_block(blockchain.chaind[0].hash, 1, "Second transaction");
             blockchain.add_block(blockchain.chaind[1].hash, 2, "Third transaction");
             for (int i = 0; i < blockchain.chaind.Count; i++)
             {
                 Console.WriteLine("Index: {0}, Prev_Hash: {1}, Hash: {2}", blockchain.chaind[i].index, blockchain.chaind[i].prev_hash, blockchain.chaind[i].hash);
-            }*/
-            Console.WriteLine("{0},{1},{2}", Block.ft_strcmp("00", "00"), Block.ft_strcmp("ab", "a"), Block.ft_strcmp("ababa", "ababab"));
+            }
+            Console.WriteLine("{0},{1},{2}", Block.ft_strcmp("00", "abcd00".Substring(3)), Block.ft_strcmp("ab", "a"), Block.ft_strcmp("ababa", "ababab"));
+            Console.WriteLine("Yolololo");
         }
     }
 }
